2b4725abd5abeb8ef601aa92798803ca9e436cc9
Tonya-Mazakova <apmazakova@gmail.com>
2018-02-22 20:03:35 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,69 @@
 class Sorter {
   constructor() {
-    // your implementation
+      this.arr = [];
+      this.arr2 = [];
+      this.indexArr = [];
+      this.compare = false;
+      this.turn = false;
   }
 
   add(element) {
-    // your implementation
+      this.arr.push(element);
   }
 
   at(index) {
-    // your implementation
-  }
+      return this.arr[index];
 
+  }
   get length() {
-    // your implementation
+    return this.arr.length;
   }
 
   toArray() {
-    // your implementation
+      return this.arr;
   }
 
   sort(indices) {
-    // your implementation
+      for(let i=0; i < indices.length; i++) {
+          for (let j = 0; j < indices.length; j++) {
+              if (this.arr[indices[j]] > this.arr[indices[j + 1]] & indices[j] < indices[j + 1]){
+                  let indexTwo = this.arr[indices[j + 1]];
+                  this.arr[indices[j + 1]] = this.arr[indices[j]];
+                  this.arr[indices[j]] = indexTwo;
+              }
+              else if(this.arr[indices[j]] < this.arr[indices[j + 1]] & indices[j] > indices[j + 1]){
+                  let indexTwo = this.arr[indices[j]];
+                  this.arr[indices[j]] = this.arr[indices[j+1]];
+                  this.arr[indices[j+1]] = indexTwo;
+              }
+          }
+      }
+      if(this.compare === true){
+          for(let i=0; i < indices.length; i++){
+              this.arr2.push(this.arr[indices[i]]);
+              this.indexArr.push(indices[i]);
+          }
+          this.setComparator();
+          this.turn = true;
+      }
+
   }
 
   setComparator(compareFunction) {
-    // your implementation
+      if(this.turn){
+          this.compare = false;
+      }
+      else{
+          this.compare = true;
+      }
+      this.turn = false;
+      this.compareFunction = compareFunction;
+      let arrSort = this.arr2.sort(this.compareFunction);
+      this.indexArr.sort();
+      for(let i=0; i < this.indexArr.length; i++){
+          this.arr.splice(this.indexArr[i], 0, arrSort[i]);
+      }
+      return this.arr;
   }
 }
 
